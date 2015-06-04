8d047273eb75472a3e70491e2cf75e8f53b9c70a
Darryl Blake <darryl@nikordi.com>
2015-06-04 15:20:43 +1200

    Tidied up, added try/catch for unstringifiableValues, all passing

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,29 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-// Single Values (Base case)
-	if (obj === null)
-		return 'null';
-	if (obj.constructor !== Object && obj.constructor !== Array) { // Test if single value
-		if (Number(obj) || obj === false || obj === true || obj === 0) // Why do I need obj === 0?
-			return String(obj);
-		else
-			return String('"' + obj + '"'); // returns just the value
-	}
-	var keyValue = ''
-// Objects
-	if (obj.constructor === Object) {
-		for (var property in obj) {
-			keyValue += '"' + property + '":' + stringifyJSON(obj[property]) + ',';
-		}
-		keyValue = '{' + keyValue.slice(0,-1) + '}'; // Adding {} and removing last comma
-	}
-// Arrays
-	if (obj.constructor == Array) {
-		obj.forEach(function(item){ // I'm assuming I can use this since it's in the Jasmine tests?
-			keyValue += stringifyJSON(item) + ',';
-		});
-		keyValue = '[' + keyValue.slice(0,-1) + ']'; // Adding {} and removing last comma
-	}
-	return keyValue;
+    // Single Values (Base case)
+    if (obj === null)
+        return 'null';
+    if (obj.constructor !== Object && obj.constructor !== Array) { // Test if single value
+        if (Number(obj) || obj === false || obj === true || obj === 0)
+            return String(obj);
+        else
+            return String('"' + obj + '"'); // returns just the value
+    }
+
+    var keyValue = '';
+    try {
+        // Objects
+        if (obj.constructor === Object) {
+            for (var property in obj) {
+                keyValue += '"' + property + '":' + stringifyJSON(obj[property]) + ',';
+            }
+            keyValue = '{' + keyValue.slice(0, -1) + '}'; // Adding {} and removing last comma
+        }
+        // Arrays
+        if (obj.constructor == Array) {
+            obj.forEach(function(item) { // I'm assuming I can use this since it's in the Jasmine tests?
+                keyValue += stringifyJSON(item) + ',';
+            });
+            keyValue = '[' + keyValue.slice(0, -1) + ']'; // Adding {} and removing last comma
+        }
+        return keyValue;
+    } catch (err) { // To catch unstringifiableValues
+        console.log(err);
+        return '{}';
+    }
 }

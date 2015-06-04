25fb61f403f7e741d83c3c90c25bb7e346d274e0
Darryl Blake <darryl@nikordi.com>
2015-06-04 14:45:51 +1200

    Getting somewhere...

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+// Single Values (Base case)
+	if (obj === null)
+		return 'null'; 
+	if (obj.constructor !== Object || obj.constructor !== Array) { // Test if single value
+		if (Number(obj))
+			return String(obj);
+		else
+			return String('"' + obj + '"'); // returns just the value
+	}
+	var keyValue = ''
+// Objects
+	if (obj.constructor === Object) {
+		for (var property in obj) {
+			keyValue += '"' + property + '":' + stringifyJSON(obj[property]) + ',';
+		}
+		keyValue = '{' + keyValue.slice(0, -1) + '}'; // Adding {} and removing last comma
+	}
+// Arrays
+	if (obj.constructor == Array) {
+		keyValue = 'ITEM'
+	}
+	return keyValue;
+}
+

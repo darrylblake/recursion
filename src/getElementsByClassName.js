c243b34df639096e391a3453b8e9880ec1d9a7f2
Darryl Blake <darryl@nikordi.com>
2015-06-05 15:19:43 +1200

    getElementsByClassName all tests passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  matches = [];
+  function getChildren(parentNode) {
+    // Base Case
+    if (parentNode.length === undefined) {
+      parentNode.classList.forEach(function(nodeClassName) {
+        if (nodeClassName === className)
+          matches.push(parentNode);
+      });
+    }
+    // Recursion
+    for (var i = 0; i < parentNode.childNodes.length; i++){ // childNodes are only array like...
+      getChildren(parentNode.childNodes[i]);
+    }    
+  }
+  getChildren(document.body);
+  return matches;
 };

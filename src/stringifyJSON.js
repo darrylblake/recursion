54b0173b87b60a9836b10d882313841e6ecbd2b8
Allen J Price <allenjprice@gmail.com>
2015-06-29 13:57:05 -0700

    Add solution code from morning lecture

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,48 @@
 
 // but you don't so you're going to write it from scratch:
 
+//inputs= some kind of data
+//outputs = that same data, wrapped in a string
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //is an array?
+  if (Array.isArray(obj)) {
+    var results = [];
+    //iterate over the array
+
+    for (var i = 0; i < obj.length; i++){
+      if ( obj[i] === undefined || typeof obj[i] === 'function'){
+        continue;
+      }
+      //recurse on each element
+      results.push( stringifyJSON(obj[i]) );
+    }
+
+    return '[' + results.join(',') + ']';
+  }
+
+  //is an object?
+  if (obj instanceof Object){
+    //stringify the object
+    var results = [];
+
+    for (var key in obj){
+      if ( obj[key] === undefined || typeof obj[key] === 'function'){
+        continue;
+      }
+      results.push(
+        stringifyJSON(key) + ":" + stringifyJSON(obj[key])
+        );
+    }
+
+    return '{' + results.join(',') + '}';
+  }
+
+  //is a string?
+  if (typeof obj === 'string'){
+    //stringify the string
+    return '"' + obj + '"';
+  }
+
+  return '' + obj;
 };
